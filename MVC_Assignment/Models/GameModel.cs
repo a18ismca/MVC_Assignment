de5bdf34556afa7a65cbe517583b993702c3c923
a18ismca <ismetcatovic@hotmail.se>
2022-08-22 11:24:59 +0200

    Done with assignment 3 of basic.

---
+++ MVC_Assignment/Models/GameModel.cs
@@ -0,0 +1,45 @@
+﻿namespace MVC_Assignment.Models
+{
+    public class GameModel
+    {
+
+        public static int CorrectAnswer { get; set; }
+
+        public static int Attempts { get; set; }
+
+
+        public static string GetResult(int guessedNumber)
+        {
+            if(guessedNumber < 0 || guessedNumber > 100)
+            {
+                return "Please enter an int between 0 and 100!";
+            } else if(guessedNumber > CorrectAnswer)
+            {
+                Attempts++;
+                return "Number is lower, try again. Attempts made: " + Attempts;
+            } else if(guessedNumber < CorrectAnswer)
+            {
+                Attempts++;
+                return "Number is higher, try again. Attempts made: " + Attempts;
+            }
+            else
+            {
+                if (Attempts > 0)
+                {
+                    return "You have guessed right. Correct answer: " + CorrectAnswer + ". Attempts made: " + Attempts;
+                }
+                return "You have guessed right. Correct answer: " + CorrectAnswer;
+
+            }
+
+        }
+
+        internal static int GetRandomNumber()
+        {
+            Random random = new Random();
+
+            return random.Next(1, 101);
+
+        }
+    }
+}
--- MVC_Assignment/Program.cs
+++ MVC_Assignment/Program.cs
@@ -2,21 +2,35 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMvc();
 
-var app = builder.Build();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(15);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
+var app = builder.Build();
 
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapControllerRoute(
+/*app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Doctor}/{action=FeverCheck}/{id?}"
+    pattern: "{controller?}/{action=GuessingGame}/{id?}"
     );
+*/
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "fever",
     pattern: "fever",
     defaults: new { controller = "Doctor", action = "FeverCheck" });
+
+app.MapControllerRoute(
+    name: "Guess",
+    pattern: "{controller=Guess}/{action=GuessingGame}/{id?}"
+    );
 
 app.Run();
