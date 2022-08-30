5eee6bc608f546fd2d7db864d1e634a74d889081
a18ismca <ismetcatovic@hotmail.se>
2022-08-30 13:04:40 +0200

    Removed session from Program.cs

---
+++ MVC_Assignment/DbData/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Assignment.DbData
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+           
+        }
+    }
+}
--- MVC_Assignment/Program.cs
+++ MVC_Assignment/Program.cs
@@ -2,28 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMvc();
 
-/*builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromSeconds(15);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
 
-*/
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 
 app.UseRouting();
-
-/*app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller?}/{action=GuessingGame}/{id?}"
-    );
-*/
-
-//app.UseSession();
 
 app.MapControllerRoute(
     name: "fever",
