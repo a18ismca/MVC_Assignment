1c6d309f6311a39998f4a7d3d82c3fb3580d517a
a18ismca <ismetcatovic@hotmail.se>
2022-10-11 12:53:56 +0200

    Added functions to the API controller.

--- MVC_Assignment/Controllers/ReactController.cs
+++ MVC_Assignment/Controllers/ReactController.cs
@@ -1,28 +1,68 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MVC_Assignment.DbData;
 using MVC_Assignment.Models;
 
-namespace MVC_Assignment.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ReactController : ControllerBase
+
+    namespace MVC_Assignment.Controllers
     {
-        readonly ApplicationDbContext _context;
-
-
-        public ReactController(ApplicationDbContext context)
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ReactController : ControllerBase
         {
-            _context = context;
-        }
+            readonly ApplicationDbContext _context;
 
-        [HttpGet]
-        public IEnumerable<Person> GetData()
-        {
-            var data = _context.People;
 
-            return data;
-        }
+            public ReactController(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            // GET /api/React/
+            [HttpGet("people")]
+            public IEnumerable<Person> GetPeople()
+            {
+
+                var data = _context.People;
+
+                return data;
+
+            }
+
+            // GET /api/React/id
+            [HttpGet("languages")]
+            public IEnumerable<Language> GetLanguages()
+            {
+                var data = _context.Languages;
+
+                return data;
+
+            }
+
+            [HttpGet("cities")]
+            public IEnumerable<City> GetCities()
+            {
+                var data = _context.Cities;
+
+                return data;
+
+            }
+
+            [HttpGet("people/{Id}")]
+            public IEnumerable<Person> GetPersonDetails(int Id)
+            {
+
+                var data = _context.People.Where(x => x.Id == Id);
+
+                return data;
+
+            }
+
+
+
+
+
     }
 }
+
+
