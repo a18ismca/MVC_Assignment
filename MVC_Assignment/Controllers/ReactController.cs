95c05dc5099e8ab853a9f22ff42eae7f28ba2fac
a18ismca <ismetcatovic@hotmail.se>
2022-10-12 16:46:42 +0200

    Modified the API controller.

--- MVC_Assignment/Controllers/ReactController.cs
+++ MVC_Assignment/Controllers/ReactController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
 using Microsoft.EntityFrameworkCore;
 using MVC_Assignment.DbData;
 using MVC_Assignment.Models;
@@ -15,21 +16,35 @@ using MVC_Assignment.Models;
 
             public ReactController(ApplicationDbContext context)
             {
-                _context = context;
+                _context = context;3
             }
 
             // GET /api/React/
             [HttpGet("people")]
-            public IEnumerable<Person> GetPeople()
+            public IEnumerable<Person> GetPeople(int id)
             {
 
-                var data = _context.People;
+            var people = _context.People;
+            var person  = _context.People.Where(x=>x.Id == id).FirstOrDefault();    
 
-                return data;
+            var personCityId = _context.People.Select(p => p.CityId).FirstOrDefault();
+
+                
+
+            //var city = _context.Cities.Where(x => x.CityId == person.CityId).FirstOrDefault();
+
+            //string cityName = city.Name;
+
+                return people;
 
             }
 
-            // GET /api/React/id
+       
+
+        /*  Where, OrderBy(Descending), ThenBy(Descending), Skip or Take operations */
+
+
+
             [HttpGet("languages")]
             public IEnumerable<Language> GetLanguages()
             {
@@ -45,18 +60,47 @@ using MVC_Assignment.Models;
                 var data = _context.Cities;
 
                 return data;
-
             }
+
+            [HttpGet("countries")]
+            public IEnumerable<Country> GetCountries()
+            {
+
+                var data = _context.Countries;
+
+                return data;
+            }
+
+            [HttpGet("{countryId}/cities")]
+            public IEnumerable<City> GetCitiesInCountry(int countryId)
+             {
+                var cities = _context.Cities.Where(x => x.CountryId == countryId);
+                return cities; 
+             }
 
             [HttpGet("people/{Id}")]
             public IEnumerable<Person> GetPersonDetails(int Id)
             {
 
-                var data = _context.People.Where(x => x.Id == Id);
+                var person = _context.People.Where(x => x.Id == Id);
+            
+                return person;
 
-                return data;
+            }// ej ::
 
+            [HttpPost("people/{name}/{phoneNumber}/{cityId}")]
+            public Person AddPerson(string name, int phoneNumber, int cityId)
+            {
+                 Person p = new Person { Name = name, PhoneNumber = phoneNumber, CityId = cityId };
+
+                _context.People.Add(p);
+
+                _context.SaveChanges();
+
+                return p;
             }
+
+            
 
 
 
